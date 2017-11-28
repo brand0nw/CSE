e4cf3121635a321c0bea1c814b605ed860e03ea1
brand0nw <31044480+brand0nw@users.noreply.github.com>
2017-11-28 10:46:31 -0800

    Added previous notes
    The ting goes grrrrrrrrrra pop pop pop pop skiby do pop pop

--- Notes.py
+++ Notes.py
@@ -1,1 +1,36 @@
-# This is working
+print("Hello World")
+
+# Brandon
+
+print(3 + 5)
+print(7 ^ 6)
+print(9/8)
+print(3 ** 2)
+
+# this makes a new line. In python 3.6, it looks like: print()
+print("See if you can figure this out")
+print(13 % 12)
+print(15 % 5)
+print(14 % 5)
+print(13 % 5)
+print(12 % 5)
+print(11 % 5)
+print(10 % 5)
+print(9 % 5)
+
+car_name = "Retractor"
+car_type = "Ford Gran Torino"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline printing
+print("I have a car called the %s. It's a %s." % (car_name, car_type))
+
+# Asking for input
+
+name = input("What is your name? ")  # In python 3, it is just called input()
+print("Hello %s." % name)
+
+age = input("How old are you? ")
+print("%s? DAYUM you old." % age)
+
