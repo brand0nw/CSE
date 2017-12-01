5b51978c5db36217e03923bfba59405ebdc70093
brand0nw <31044480+brand0nw@users.noreply.github.com>
2017-12-01 11:35:26 -0800

    Added stuff
    12-1-17

--- Brandon Widjaja - Guessgame.py
+++ Brandon Widjaja - Guessgame.py
@@ -1,1 +1,8 @@
-# Brandon Widjaja
+import random
+
+
+print("Guess a number between 0-50.")
+print("You have 5 tries.")
+print("Have fun.")
+
+print(random.randint(0, 50))
--- Notes.py
+++ Notes.py
@@ -1,124 +1,124 @@
-import random
-# print("Hello World")
+# import random
+# # print("Hello World")
+# #
+# # # Brandon
+# #
+# # print(3 + 5)
+# # print(7 ^ 6)
+# # print(9/8)
+# # print(3 ** 2)
+# #
+# # # this makes a new line. In python 3.6, it looks like: print()
+# # print("See if you can figure this out")
+# # print(13 % 12)
+# # print(15 % 5)
+# # print(14 % 5)
+# # print(13 % 5)
+# # print(12 % 5)
+# # print(11 % 5)
+# # print(10 % 5)
+# # print(9 % 5)
+# #
+# # car_name = "Retractor"
+# # car_type = "Ford Gran Torino"
+# # car_cylinders = 8
+# # car_mpg = 9000.1
+# #
+# # # Inline printing
+# # print("I have a car called the %s. It's a %s." % (car_name, car_type))
+# #
+# # # Asking for input
+# #
+# # name = input("What is your name? ")  # In python 3, it is just called input()
+# # print("Hello %s." % name)
+# #
+# # age = input("How old are you? ")
+# # print("%s?! DAYUM you old." % age)
+# #
 #
-# # Brandon
-#
-# print(3 + 5)
-# print(7 ^ 6)
-# print(9/8)
-# print(3 ** 2)
-#
-# # this makes a new line. In python 3.6, it looks like: print()
-# print("See if you can figure this out")
-# print(13 % 12)
-# print(15 % 5)
-# print(14 % 5)
-# print(13 % 5)
-# print(12 % 5)
-# print(11 % 5)
-# print(10 % 5)
-# print(9 % 5)
-#
-# car_name = "Retractor"
-# car_type = "Ford Gran Torino"
-# car_cylinders = 8
-# car_mpg = 9000.1
-#
-# # Inline printing
-# print("I have a car called the %s. It's a %s." % (car_name, car_type))
-#
-# # Asking for input
-#
-# name = input("What is your name? ")  # In python 3, it is just called input()
-# print("Hello %s." % name)
-#
-# age = input("How old are you? ")
-# print("%s?! DAYUM you old." % age)
-#
-
-# Functions
-
-
-# def print_hw():
-#     print("Hello World")
+# # Functions
 #
 #
-# print_hw()
-# print_hw()
-# print_hw()
+# # def print_hw():
+# #     print("Hello World")
+# #
+# #
+# # print_hw()
+# # print_hw()
+# # print_hw()
+# #
+# #
+# # def say_hi(name):  # name is a parameter
+# #     print("Hello %s" % name)
+# #     print("Enjoy your day.")
+# #
+# #
+# # say_hi("John")
+# #
+# #
+# # def print_age(name, age):
+# #     print("%s is %d years old." % (name, age))
+# #     age += 1  # age = age + 1
+# #     print("Next year, they will be %d" % age)
+# #
+# #
+# # print_age("John", 15)
+# #
+# #
+# # def f(x):
+# #     return x**3 + 4 * x**2 + 7 * x - 4
+# #
+# #
+# # print(f(3))
+# # print(f(4))
+# # print(f(5))
+# #
+# #
+# # # If statements
+# #
+# #
+# # def grade_calc(percentage):
+# #     if percentage >= 90:
+# #         return "A"
+# #     elif percentage >= 80:
+# #         return "B"
+# #     elif percentage >= 70:
+# #         return "C"
+# #     elif percentage >= 60:
+# #         return "D"
+# #     else :
+# #         return "F"
 #
 #
-# def say_hi(name):  # name is a parameter
-#     print("Hello %s" % name)
-#     print("Enjoy your day.")
+# # Function
 #
 #
-# say_hi("John")
+# def happy_bday(name):
+#     print("Happy Birthday to you" + ",")
+#     print("Happy Birthday to you" + ",")
+#     print("Happy Birthday to " + name + ",")
+#     print("Happy Birthday to you" + ".")
 #
 #
-# def print_age(name, age):
-#     print("%s is %d years old." % (name, age))
-#     age += 1  # age = age + 1
-#     print("Next year, they will be %d" % age)
+# happy_bday("Mr.Grinch")
 #
 #
-# print_age("John", 15)
+# # Loops
+#
+# for num in range(10):
+#     print(num + 1)
+#
+# # DO NOT RUN!!!
 #
 #
-# def f(x):
-#     return x**3 + 4 * x**2 + 7 * x - 4
+# # a = 1
+# # while a <= 10:
+# #     print(a)
+# #     a += 1
 #
 #
-# print(f(3))
-# print(f(4))
-# print(f(5))
+# # Random Number
 #
-#
-# # If statements
-#
-#
-# def grade_calc(percentage):
-#     if percentage >= 90:
-#         return "A"
-#     elif percentage >= 80:
-#         return "B"
-#     elif percentage >= 70:
-#         return "C"
-#     elif percentage >= 60:
-#         return "D"
-#     else :
-#         return "F"
-
-
-# Function
-
-
-def happy_bday(name):
-    print("Happy Birthday to you" + ",")
-    print("Happy Birthday to you" + ",")
-    print("Happy Birthday to " + name + ",")
-    print("Happy Birthday to you" + ".")
-
-
-happy_bday("Mr.Grinch")
-
-
-# Loops
-
-for num in range(10):
-    print(num + 1)
-
-# DO NOT RUN!!!
-
-
-a = 1
-while a <= 10:
-    print(a)
-    a += 1
-
-
-# Random Number
-
- # This should be on line 1
-print(random.randint(0,100))
+#  # This should be on line 1
+# print(random.randint(0,100))
 
