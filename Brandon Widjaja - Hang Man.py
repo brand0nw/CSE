18459c9726a5bb678685806e788eb94b94e951c1
brand0nw <31044480+brand0nw@users.noreply.github.com>
2018-01-23 10:47:34 -0800

    New stuff
    Moar stuf

--- Brandon Widjaja - Hang Man.py
+++ Brandon Widjaja - Hang Man.py
@@ -5,13 +5,36 @@ import string
 A general guide for Hangman
 1. Make a word bank - 10 items
 2. Pick a random item from the list
-3. Hide the word (use *)
+3. Add a guess to the list of letters guessed
 4. Reveal letters already guessed
 5. Create the win condition
-
 """
-
-word_bank = ["Mr.Wiebe", "Cheese", "Charlie", "Daniels", "Kenshiro", "Edison", "Georgia", "Liberty", "Trombone", "Computer"]
-
+print("Welcome to Hangman.")
+the_count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+word_bank = ["hot", "cheese", "charlie", "daniels", "kenshiro", "edison", "georgia", "liberty", "trombone", "computer"]
+guesses_left = 10
+word = random.choice(word_bank)
 print(len(word_bank))
 print(word_bank)
+print(word)
+letters_guessed = []
+player_guess = "True"
+while word != player_guess:
+    output = []
+    guess = input()
+    if guess == 'quit':
+        exit(0)
+    letters_guessed.append(guess)
+    letter = string.ascii_lowercase
+    print(letters_guessed)
+    print(output)
+
+    if letter == guess:
+
+        output.append(guess)
+    # if guess != letter:
+    #     guesses_left -= 1
+    #     print("%s guesses left." % guesses_left)
+    #
+    # if guess = letter:
+    #     print()
