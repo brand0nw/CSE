48c8eff4f50b75fb79e58cd5b9af502177fba626
brand0nw <31044480+brand0nw@users.noreply.github.com>
2018-01-24 10:48:08 -0800

    Hangman
    Toua doesn't tell me what to do

--- Brandon Widjaja - Hang Man.py
+++ Brandon Widjaja - Hang Man.py
@@ -9,32 +9,50 @@ A general guide for Hangman
 4. Reveal letters already guessed
 5. Create the win condition
 """
+
 print("Welcome to Hangman.")
 the_count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 word_bank = ["hot", "cheese", "charlie", "daniels", "kenshiro", "edison", "georgia", "liberty", "trombone", "computer"]
 guesses_left = 10
+
 word = random.choice(word_bank)
-print(len(word_bank))
-print(word_bank)
-print(word)
+
 letters_guessed = []
 player_guess = "True"
+
+print("You have 10 guesses.")
+
 while word != player_guess:
+
     output = []
-    guess = input()
-    if guess == 'quit':
-        exit(0)
-    letters_guessed.append(guess)
-    letter = string.ascii_lowercase
-    print(letters_guessed)
+
+    for letter in word:
+        if letter in letters_guessed:
+            output.append(letter)
+
+        else:
+            output.append("*")
     print(output)
 
-    if letter == guess:
+    current_guess = input()
 
-        output.append(guess)
-    # if guess != letter:
-    #     guesses_left -= 1
-    #     print("%s guesses left." % guesses_left)
-    #
-    # if guess = letter:
-    #     print()
+    if current_guess == 'quit':
+        print("Game Over")
+        exit(0)
+
+    if current_guess not in word:
+        guesses_left -= 1
+        print("%s guesses left." % guesses_left)
+
+    if guesses_left == 0:
+        print("Game Over")
+        exit(0)
+
+    letters_guessed.append(current_guess)
+
+    print(letters_guessed)
+
+    if output == :
+        print("You Win!")
+        print("Game Over")
+        exit(0)
