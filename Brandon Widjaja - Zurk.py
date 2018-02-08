ee9f6d73491e47da9b2a6f01fdffbae39f01da95
brand0nw <31044480+brand0nw@users.noreply.github.com>
2018-02-08 10:46:55 -0800

    Zurk
    Brandon Widjaja

---
+++ Brandon Widjaja - Zurk.py
@@ -0,0 +1,31 @@
+world_map = {
+    'ROOM': {
+        'NAME': "Room",
+        'DESCRIPTION': "You are in a room with a torn up couch and claw marks on a wall.",
+        'PATHS': {
+            'NORTHEAST': 'STEELMILL',
+            'SOUTH': 'ECHOINGROOM',
+            'EAST': 'LEGOROOM',
+    'STEELMILL': {
+        'NAME': "Steel Mill",
+        'DESCRIPTION': "You enter a room with the Orc Warlord Ukifak Lasgoni.",
+        'PATHS': {
+            'WEST': 'EMPTYROOM',
+    'EMPTYROOM': {
+        'NAME': "Empty Room",
+        'DESCRIPTION': "You enter an empty room all that is there is a button.",
+        'PATHS':{
+            'EAST': 'STEELMILL',
+    'ECHOINGROOM': {
+        'NAME': "Echoing Room"
+    }
+
+        }
+
+    }
+        }
+    }
+
+        }
+    }
+}
