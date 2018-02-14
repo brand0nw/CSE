ed0feb236f0dfed53b6e6d0c45c08ba15fdd3cf1
brand0nw <31044480+brand0nw@users.noreply.github.com>
2018-02-14 10:47:08 -0800

    Zurk
    Brandon Widjaja

--- Brandon Widjaja - Zurk.py
+++ Brandon Widjaja - Zurk.py
@@ -19,7 +19,7 @@ world_map = {
     'EMPTYROOM': {
         'NAME': "Empty Room",
         'DESCRIPTION': "You enter an empty room all that is there is a button.",
-        'PATHS':{
+        'PATHS': {
             'EAST': 'STEELMILL',
         }
     },
@@ -35,8 +35,7 @@ world_map = {
     'WELLLITROOM': {
         'NAME': "Well Lit Room",
         'DESCRIPTION': "You enter a well lit room with a heavily armored JoJo.\n"
-                       "He seems aggressive as he mutters 'Omae "
-                       "wa moe shinderu'",
+                       "He seems aggressive as he mutters 'Omae wa moe shinderu'.",
         'PATHS': {
             'EAST': 'ECHOINGROOM',
             'WEST': 'VAULT'
@@ -50,5 +49,25 @@ world_map = {
         'PATHS': {
             ''
         }
-    }
+    },
 }
+current_node = world_map['ROOM']
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'NORTHWEST', 'NORTHEAST', 'SOUTHWEST', 'SOUTHEAST']
+
+while True:
+
+    print(current_node['NAME'])
+    print(current_node['DESCRIPTION'])
+
+    command = input('>_')
+    if command == 'quit':
+        quit(0)
+    if command in directions:
+        try:
+            name_of_node = current_node['PATHS'][command]
+            current_node = world_map[name_of_node]
+        except KeyError:
+            print("You cannot go that way.")
+    else:
+        print('Command Not Recognized.')
+    print()
